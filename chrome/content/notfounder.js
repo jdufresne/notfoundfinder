62c2a03d4edc0e3276b72aaa7ca71896be5e0458
Jon <jon@bender.futurama>
2011-03-20 17:56:02 -0700

    Get window to work and sync with state of the request observer

--- chrome/content/notfounder.js
+++ chrome/content/notfounder.js
@@ -1,18 +1,26 @@
 function NotFounder() {
-	this.notFoundRequests = new Array();
-	this.callback = null;
+	this.register();
 }
 
 NotFounder.prototype = {
+	requests: [],
+	updateCallback: null,
+
 	setUpdateCallback: function(callback) {
 		this.updateCallback = callback;
 	},
 
-	observe: function(subject, topic, data) {
-		subject.QueryInterface(Components.interfaces.nsIHttpChannel);
-		if (subject.responseStatus == 404) {
-			this.notFoundRequests.push(subject);
-			this.updateCallback();
+	clearRequests: function() {
+		this.requests = [];
+	},
+
+	observe: function(request, topic, data) {
+		request.QueryInterface(Components.interfaces.nsIHttpChannel);
+		if (request.responseStatus != 200) {
+			this.requests.push(request);
+			if (this.updateCallback) {
+				this.updateCallback();
+			}
 		}
 	},
 
@@ -34,16 +42,20 @@ NotFounder.prototype = {
 
 
 function NotFounderController() {
-	this.observer = new NotFounder();
-	var controller = this;
-	this.observer.setUpdateCallback(function() { controller.update(); });
-	window.addEventListener('load', function() { controller.init(); }, false);
-	window.addEventListener('unload', function() { controller.shutdown(); }, false);
+	var self = this;
+	window.addEventListener('load', function() { self.init(); }, false);
+	window.addEventListener('unload', function() { self.shutdown(); }, false);
 }
 
 NotFounderController.prototype = {
+	window: null,
+
 	init: function() {
-		this.observer.register();
+		var self = this;
+		var status = document.getElementById('nff-status');
+		status.addEventListener('click', function() { self.openWindow(); }, false);
+		this.observer = new NotFounder();
+		this.observer.setUpdateCallback(function() { self.update(); });
 	},
 
 	shutdown: function() {
@@ -51,11 +63,80 @@ NotFounderController.prototype = {
 	},
 
 	update: function() {
-		var panel = document.getElementById('not-founder-panel');
-		var num = this.observer.notFoundRequests.length;
-		panel.label = "Not Found (" + num + ")";
+		var status = document.getElementById('nff-status');
+		var num = this.observer.requests.length;
+		status.label = "Not Found (" + num + ")";
+
 		if (num) {
-		 	panel.className = 'warning';
+		 	status.setAttribute('class', 'warning');
+		} else {
+			status.removeAttribute('class');
+		}
+
+		if (this.window) {
+			this.updateWindow();
+		}
+	},
+
+	openWindow: function() {
+		this.window = window.open(
+			'chrome://notfounder/content/window.xul',
+			'requests',
+			'chrome,centerscreen'
+		);
+
+		var self = this;
+		this.window.addEventListener('load', function() {
+			var button = self.window.document.getElementById('nff-clear-button');
+			button.addEventListener('command', function() {
+				self.observer.clearRequests();
+				self.update();
+			}, false);
+			self.updateWindow();
+		}, false);
+		this.window.addEventListener('close', function() {
+			self.window = null;
+		}, false);
+	},
+
+	updateWindow: function() {
+		var table = this.window.document.getElementById('nff-request-table');
+		var nodes = [];
+
+		for (var i = 0; i < table.childNodes.length; i++) {
+			if (table.childNodes[i].nodeName == 'listitem') {
+				nodes.push(table.childNodes[i]);
+			}
+		}
+
+		while (nodes.length) {
+			table.removeChild(nodes.pop());
+		}
+
+		for (var i = 0; i < this.observer.requests.length; i++) {
+			var request = this.observer.requests[i];
+			var row = this.window.document.createElement('listitem');
+			var item;
+
+			item = this.window.document.createElement('listcell');
+			item.setAttribute('label', request.URI.spec);
+			row.appendChild(item);
+
+			item = this.window.document.createElement('listcell');
+			item.setAttribute(
+				'label',
+				request.responseStatus + " " + request.responseStatusText
+			);
+			row.appendChild(item);
+
+			item = this.window.document.createElement('listcell');
+			item.setAttribute(
+				'label',
+				request.requestMethod
+			);
+			row.appendChild(item);
+
+			table.appendChild(row);
 		}
 	},
 }
